5c6331a41c88329dbe2c20925e6e09889725317c
ceegeechow <camille@chowww.com>
2018-02-19 16:37:30 -0500

    compiles and runs on Linux

--- portscanner.cpp
+++ portscanner.cpp
@@ -5,11 +5,12 @@
 #include <string>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 //checks if port on given address is open
 bool port_is_open(const struct sockaddr_in addr)
 {
-    int sfd = socket(AF_INET, SOCK_STREAM, 6); 		//right type??? use getprotoent or 0??
+    int sfd = socket(AF_INET, SOCK_DGRAM, 6); 		//SOCK_DGRAM??? use getprotoent or 0??
     if ((connect(sfd, (struct sockaddr *) &addr, sizeof(addr))) == 0)
 		return true;
 	return false;
@@ -22,9 +23,10 @@ int main (int argc, char** argv)
 	int stop = 1024;
 	char* hostname;
 
+	//check for input arguments
 	if (argc <= 1)
 	{
-		std::cerr << "Error: Not enough input arguments";
+		std::cerr << "Error: Not enough input arguments\n";
 		exit(0);
 	}
 
@@ -35,8 +37,8 @@ int main (int argc, char** argv)
 		{
 			std::string str = optarg;
 			char* s = const_cast<char*>(str.c_str());
-			start = atoi(std::strtok(s, ":"));
-			stop = atoi(std::strtok(NULL, ":"));
+			start = atoi(strtok(s, ":"));
+			stop = atoi(strtok(NULL, ":"));
 		}
 	}
 	if (optind >= argc)
@@ -56,13 +58,15 @@ int main (int argc, char** argv)
 	memcpy(&server.sin_addr, he->h_addr_list[0], he->h_length);
   	server.sin_family = AF_INET;
 
+  	std::cout<< "Starting scan of ports " << start << " through " << stop << "\n";
+
 	//scan range of ports
 	for (int i = start; i <= stop; i++)
 	{
-		server.sin_port = htons(i);
+		server.sin_port = htons(i); //set port number
 		if (port_is_open(server))
 		{
-			std::cout << "Port: " << i << " is open\n";
+			std::cout << "Port " << i << " is open\n";
 		}
 	}
 	std::cout << "Finished scanning ports " << start << " through " << stop << "\n";
